1f25e0917fbea69a0acc1c1484d7ea39a5b48bf3
mamgjunior <mamgjunior@hotmail.com>
2021-12-10 15:52:07 -0300

    melhorando o carrinho

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import Settings, settings
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [    
     path('admin/', admin.site.urls),    
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
 
     # Os padrões de urls acima são mais restritivos que esse por esse motivo estão acima
     path('', include('shop.urls', namespace='shop')),
